53fb5eb945ef560169525e467f04d608986e9e0a
Joctan Gutierrez <joctangaelmorenogutierrez@gmail.com>
2026-04-24 18:26:16 -0600

    Add files via upload
    A basic port scanner written in Python that allows users to input an IP address and scans ports from 1 to 65535. It identifies whether each port is open or closed using socket connections, making it a simple tool for learning network fundamentals and security concepts.

---
+++ DetectrodePuertos.py
@@ -0,0 +1,21 @@
+import socket
+
+ip = input ("Ingresa la dirección IP:")
+
+for puerto in range(1,65535):
+
+    sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(5)
+
+    result= sock.connect_ex((ip, puerto))
+
+    if result == 0:
+        print("Puerto Abierto: " +str(puerto))
+        sock.close()
+        
+    else:
+        print("Puerto Cerrado: " + str(puerto))
+
+
+
+
